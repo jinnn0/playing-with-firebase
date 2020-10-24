a2df06a3e6bc1ccd835eee6523d779aa4dc4cd01
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-10-24 22:33:33 +0100

    Updates

---
+++ precache-manifest.1ddc615312a55260dfae738f955452c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed97d140208c905744058bdc72ccad4",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "cb38c9f3a871ef4ec76c",
+    "url": "/firegram/static/css/main.4269e66b.chunk.css"
+  },
+  {
+    "revision": "9d96ecd2916f64c19123",
+    "url": "/firegram/static/js/2.8ca0ac48.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/firegram/static/js/2.8ca0ac48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb38c9f3a871ef4ec76c",
+    "url": "/firegram/static/js/main.ba4692a7.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
